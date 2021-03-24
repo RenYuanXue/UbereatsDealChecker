5df3dc3148133f646708173d8b08819e74f43a18
PTX <peterxue2011@gmail.com>
2021-03-24 00:55:02 -0400

    Making Heroku to port at 5000

--- app/main.py
+++ app/main.py
@@ -63,4 +63,4 @@ def result(input_address, selected_category, selected_promotion):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
